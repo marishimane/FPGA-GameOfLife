d57cd182ab57f20b26d5e6166f93104db2ec36f4
FranRoth <franciscoroth@amalgama.co>
2023-07-06 21:38:56 -0300

    Parametrizacion v2

--- board.vhd
+++ board.vhd
@@ -3,98 +3,81 @@ use IEEE.STD_LOGIC_1164.ALL;
 use work.cell;
 
 entity board is
+    generic(N : natural := 16);
     port (
         clk : in STD_LOGIC;
-        values : in STD_LOGIC_VECTOR(0 to 8);
+        values : in STD_LOGIC_VECTOR(0 to (N*N - 1) );
         enable_set : in STD_LOGIC;
         enable_run : in std_logic;
-        out_o : out STD_LOGIC_VECTOR(0 to 8)
+        out_o : out STD_LOGIC_VECTOR(0 to (N*N - 1) )
     );
 end board;
 
 architecture Behavioral of board is
-    signal cell_1_out : std_logic;
-    signal cell_2_out : std_logic;
-    signal cell_3_out : std_logic;
-    signal cell_4_out : std_logic;
-    signal cell_5_out : std_logic;
-    signal cell_6_out : std_logic;
-    signal cell_7_out : std_logic;
-    signal cell_8_out : std_logic;
-    signal cell_9_out : std_logic;
-
-    type board_t is array(2 downto 0) of std_logic_vector(2 downto 0);
-    type row is array(2 downto 0) of std_logic_vector(7 downto 0);
-    type neighbors_t is array(2 downto 0) of row;
+    type board_t is array(N-1 downto 0) of std_logic_vector(N-1 downto 0);
+    type row is array(N-1 downto 0) of std_logic_vector(7 downto 0);
+    type neighbors_t is array(N-1 downto 0) of row;
 
     signal cells_out : board_t;
     signal cells_neighbors : neighbors_t;
     signal cells_values : board_t;
 
-    signal neighbors1 : std_logic_vector(7 downto 0);
-	signal neighbors2 : std_logic_vector(7 downto 0);
-	signal neighbors3 : std_logic_vector(7 downto 0);
-	signal neighbors4 : std_logic_vector(7 downto 0);
-	signal neighbors5 : std_logic_vector(7 downto 0);
-	signal neighbors6 : std_logic_vector(7 downto 0);
-	signal neighbors7 : std_logic_vector(7 downto 0);
-	signal neighbors8 : std_logic_vector(7 downto 0);
-	signal neighbors9 : std_logic_vector(7 downto 0);
-
-    
 begin
-    input_cells_values_row: for y in 0 to 2 generate
-      input_cells_values_col: for x in 0 to 2 generate
-        cells_values(y)(x) <= values(3*y + x);
+    input_cells_values_row: for y in 0 to (N-1) generate
+      input_cells_values_col: for x in 0 to (N-1) generate
+        cells_values(y)(x) <= values(N*y + x);
       end generate input_cells_values_col;
     end generate input_cells_values_row;
 
-    output_cells_row: for y in 0 to 2 generate
-      output_cells_col: for x in 0 to 2 generate
-        out_o(3*y + x) <= cells_out(y)(x);
+    output_cells_row: for y in 0 to (N-1) generate
+      output_cells_col: for x in 0 to (N-1) generate
+        out_o(N*y + x) <= cells_out(y)(x);
       end generate output_cells_col;
     end generate output_cells_row;
     
+    -- TOP LEFT
+    cells_neighbors(0)(0)(0) <= '0';
+    cells_neighbors(0)(0)(1) <= '0';
+    cells_neighbors(0)(0)(2) <= '0';
+    cells_neighbors(0)(0)(3) <= '0';
+    cells_neighbors(0)(0)(4) <= cells_out(0)(1);
+    cells_neighbors(0)(0)(5) <= '0';
+    cells_neighbors(0)(0)(6) <= cells_out(1)(0);
+    cells_neighbors(0)(0)(7) <= cells_out(1)(1);
 
-        cells_neighbors(0)(0)(0) <= '0';
-        cells_neighbors(0)(0)(1) <= '0';
-        cells_neighbors(0)(0)(2) <= '0';
-        cells_neighbors(0)(0)(3) <= '0';
-        cells_neighbors(0)(0)(4) <= cells_out(0)(1);
-        cells_neighbors(0)(0)(5) <= '0';
-        cells_neighbors(0)(0)(6) <= cells_out(1)(0);
-        cells_neighbors(0)(0)(7) <= cells_out(1)(1);
-
-        cells_neighbors(0)(2)(0) <= '0';
-        cells_neighbors(0)(2)(1) <= '0';
-        cells_neighbors(0)(2)(2) <= '0';
-        cells_neighbors(0)(2)(3) <= '0';
-        cells_neighbors(0)(2)(4) <= cells_out(0)(1);
-        cells_neighbors(0)(2)(5) <= '0';
-        cells_neighbors(0)(2)(6) <= cells_out(1)(1);
-        cells_neighbors(0)(2)(7) <= cells_out(1)(2);
-
-
-        cells_neighbors(2)(0)(0) <= '0';
-        cells_neighbors(2)(0)(1) <= cells_out(1)(0);
-        cells_neighbors(2)(0)(2) <= cells_out(1)(1);
-        cells_neighbors(2)(0)(3) <= '0';
-        cells_neighbors(2)(0)(4) <= cells_out(0)(1);
-        cells_neighbors(2)(0)(5) <= '0';
-        cells_neighbors(2)(0)(6) <= '0';
-        cells_neighbors(2)(0)(7) <= '0';
+    -- TOP RIGHT
+    cells_neighbors(0)(N-1)(0) <= '0';
+    cells_neighbors(0)(N-1)(1) <= '0';
+    cells_neighbors(0)(N-1)(2) <= '0';
+    cells_neighbors(0)(N-1)(3) <= '0';
+    cells_neighbors(0)(N-1)(4) <= cells_out(0)(N-2);
+    cells_neighbors(0)(N-1)(5) <= '0';
+    cells_neighbors(0)(N-1)(6) <= cells_out(1)(N-2);
+    cells_neighbors(0)(N-1)(7) <= cells_out(1)(N-1);
 
 
-        cells_neighbors(2)(2)(0) <= cells_out(1)(1);
-        cells_neighbors(2)(2)(1) <= cells_out(1)(2);
-        cells_neighbors(2)(2)(2) <= '0';
-        cells_neighbors(2)(2)(3) <= cells_out(2)(1);
-        cells_neighbors(2)(2)(4) <= '0';
-        cells_neighbors(2)(2)(5) <= '0';
-        cells_neighbors(2)(2)(6) <= '0';
-        cells_neighbors(2)(2)(7) <= '0';
+    -- BOTTOM LEFT
+    cells_neighbors(N-1)(0)(0) <= '0';
+    cells_neighbors(N-1)(0)(1) <= cells_out(N-2)(0);
+    cells_neighbors(N-1)(0)(2) <= cells_out(N-2)(1);
+    cells_neighbors(N-1)(0)(3) <= '0';
+    cells_neighbors(N-1)(0)(4) <= cells_out(N-1)(1);
+    cells_neighbors(N-1)(0)(5) <= '0';
+    cells_neighbors(N-1)(0)(6) <= '0';
+    cells_neighbors(N-1)(0)(7) <= '0';
 
-    cell_neighbors_top_border: for x in 1 to 1 generate
+
+    -- BOTTOM RIGHT
+    cells_neighbors(N-1)(N-1)(0) <= cells_out(N-2)(N-2);
+    cells_neighbors(N-1)(N-1)(1) <= cells_out(N-2)(N-1);
+    cells_neighbors(N-1)(N-1)(2) <= '0';
+    cells_neighbors(N-1)(N-1)(3) <= cells_out(N-1)(N-2);
+    cells_neighbors(N-1)(N-1)(4) <= '0';
+    cells_neighbors(N-1)(N-1)(5) <= '0';
+    cells_neighbors(N-1)(N-1)(6) <= '0';
+    cells_neighbors(N-1)(N-1)(7) <= '0';
+
+    cell_neighbors_top_border: for x in 1 to N-2 generate
         cells_neighbors(0)(x)(0) <= '0';
         cells_neighbors(0)(x)(1) <= '0';
         cells_neighbors(0)(x)(2) <= '0';
@@ -105,18 +88,18 @@ begin
         cells_neighbors(0)(x)(7) <= cells_out(1)(x+1);
     end generate cell_neighbors_top_border;
 
-    cell_neighbors_bottom_border: for x in 1 to 1 generate
-        cells_neighbors(2)(x)(0) <= cells_out(1)(x-1);
-        cells_neighbors(2)(x)(1) <= cells_out(1)(x);
-        cells_neighbors(2)(x)(2) <= cells_out(1)(x+1);
-        cells_neighbors(2)(x)(3) <= cells_out(2)(x-1);
-        cells_neighbors(2)(x)(4) <= cells_out(2)(x+1);
-        cells_neighbors(2)(x)(5) <= '0';
-        cells_neighbors(2)(x)(6) <= '0';
-        cells_neighbors(2)(x)(7) <= '0';
+    cell_neighbors_bottom_border: for x in 1 to N-2 generate
+        cells_neighbors(N-1)(x)(0) <= cells_out(N-2)(x-1);
+        cells_neighbors(N-1)(x)(1) <= cells_out(N-2)(x);
+        cells_neighbors(N-1)(x)(2) <= cells_out(N-2)(x+1);
+        cells_neighbors(N-1)(x)(3) <= cells_out(N-1)(x-1);
+        cells_neighbors(N-1)(x)(4) <= cells_out(N-1)(x+1);
+        cells_neighbors(N-1)(x)(5) <= '0';
+        cells_neighbors(N-1)(x)(6) <= '0';
+        cells_neighbors(N-1)(x)(7) <= '0';
     end generate cell_neighbors_bottom_border;
 
-    cell_neighbors_left_border: for y in 1 to 1 generate
+    cell_neighbors_left_border: for y in 1 to N-2 generate
         cells_neighbors(y)(0)(0) <= '0';
         cells_neighbors(y)(0)(1) <= cells_out(y-1)(0);
         cells_neighbors(y)(0)(2) <= cells_out(y-1)(1);
@@ -127,20 +110,20 @@ begin
         cells_neighbors(y)(0)(7) <= cells_out(y+1)(1);
     end generate cell_neighbors_left_border;
 
-    cell_neighbors_right_border: for y in 1 to 1 generate
-        cells_neighbors(y)(2)(0) <= cells_out(y-1)(1);
-        cells_neighbors(y)(2)(1) <= cells_out(y-1)(2);
-        cells_neighbors(y)(2)(2) <= '0';
-        cells_neighbors(y)(2)(3) <= cells_out(y)(1);
-        cells_neighbors(y)(2)(4) <= '0';
-        cells_neighbors(y)(2)(5) <= cells_out(y+1)(1);
-        cells_neighbors(y)(2)(6) <= cells_out(y+1)(2);
-        cells_neighbors(y)(2)(7) <= '0';
+    cell_neighbors_right_border: for y in 1 to N-2 generate
+        cells_neighbors(y)(N-1)(0) <= cells_out(y-1)(N-2);
+        cells_neighbors(y)(N-1)(1) <= cells_out(y-1)(N-1);
+        cells_neighbors(y)(N-1)(2) <= '0';
+        cells_neighbors(y)(N-1)(3) <= cells_out(y)(N-2);
+        cells_neighbors(y)(N-1)(4) <= '0';
+        cells_neighbors(y)(N-1)(5) <= cells_out(y+1)(N-2);
+        cells_neighbors(y)(N-1)(6) <= cells_out(y+1)(N-1);
+        cells_neighbors(y)(N-1)(7) <= '0';
     end generate cell_neighbors_right_border;
 
 
-    cell_neighbors_rows: for y in 1 to 1 generate
-      cell_neighbors_cols: for x in 1 to 1 generate
+    cell_neighbors_rows: for y in 1 to N-2 generate
+      cell_neighbors_cols: for x in 1 to N-2 generate
         cells_neighbors(y)(x)(0) <= cells_out(y-1)(x-1);
         cells_neighbors(y)(x)(1) <= cells_out(y-1)(x  );
         cells_neighbors(y)(x)(2) <= cells_out(y-1)(x+1);
@@ -152,8 +135,8 @@ begin
       end generate cell_neighbors_cols;
     end generate cell_neighbors_rows;
 
-    cell_rows: for y in 0 to 2 generate
-      cell_cols: for x in 0 to 2 generate
+    cell_rows: for y in 0 to N-1 generate
+      cell_cols: for x in 0 to N-1 generate
         cell_y_x: entity work.cell
           port map(
             clk => clk,
--- board_test.vhd
+++ board_test.vhd
@@ -10,12 +10,12 @@ architecture tester_architecture of tester is
 
     --Inputs
     signal clk_i : std_logic := '1';
-	signal values: std_logic_vector(0 to 8);
+	signal values: std_logic_vector(0 to 255);
     signal enable_set : std_logic := '0';
 	signal enable_run : std_logic := '0';
     
     --Outputs
-	signal out_o : std_logic_vector(0 to 8);
+	signal out_o : std_logic_vector(0 to 255);
     
     -- Clock period definitions
     constant clk_period : time := 1 us;
@@ -33,21 +33,39 @@ architecture tester_architecture of tester is
     -- clk_process : process
     -- begin
 
-    values <= "010001111";
+    values <= "1001010010101100011010010110100111010100100110100010110100110101100100110100110001101001101001101101010011010011001010110010110110010100100101100110101101101001110100101001101000101101001101011001001101001100011010011010011011010100110100110010101100101101";
 
     CLOCK:
     process 
     begin
         enable_set <= '1';
+
         clk_i <= '0';
         wait for 10 ns;
         clk_i <= '1';
         wait for 10 ns;
+
         clk_i <= '0';
         wait for 10 ns;
         enable_set <= '0';
         enable_run <= '1';
         wait for 10 ns;
+
+        clk_i <= '1';
+        wait for 10 ns;
+        clk_i <= '0';
+        wait for 10 ns;
+
+        clk_i <= '1';
+        wait for 10 ns;
+        clk_i <= '0';
+        wait for 10 ns;
+
+        clk_i <= '1';
+        wait for 10 ns;
+        clk_i <= '0';
+        wait for 10 ns;
+
         clk_i <= '1';
         wait for 10 ns;
         clk_i <= '0';
